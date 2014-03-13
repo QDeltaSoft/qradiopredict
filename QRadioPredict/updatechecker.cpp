efcaadda0e0618745eac62bfb9bdff0374419ed8
Adrian Musceac <kantooon@gmail.com>
2014-03-13 10:07:14 +0200

    Added new gitignore pattern

--- QRadioPredict/updatechecker.cpp
+++ QRadioPredict/updatechecker.cpp
@@ -3,4 +3,25 @@
 UpdateChecker::UpdateChecker(QObject *parent) :
     QObject(parent)
 {
+    _socket = new QTcpSocket;
+    QObject::connect(_socket,SIGNAL(error(QAbstractSocket::SocketError)),
+                     this,SLOT(connectionError()));
+    QObject::connect(_socket,SIGNAL(connected()),this,
+                     SLOT(readyWrite()));
+}
+
+
+UpdateChecker::~UpdateChecker()
+{
+    delete _socket;
+}
+
+void UpdateChecker::connectionError()
+{
+
+}
+
+void UpdateChecker::readyWrite()
+{
+
 }
--- QRadioPredict/updatechecker.h
+++ QRadioPredict/updatechecker.h
@@ -2,16 +2,24 @@
 #define UPDATECHECKER_H
 
 #include <QObject>
+#include <QTcpSocket>
 
 class UpdateChecker : public QObject
 {
     Q_OBJECT
 public:
     explicit UpdateChecker(QObject *parent = 0);
-    
+    ~UpdateChecker();
 signals:
     
 public slots:
+
+private slots:
+    void connectionError();
+    void readyWrite();
+
+private:
+    QTcpSocket *_socket;
     
 };
 
